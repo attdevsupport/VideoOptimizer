47ada6def6d375b0c59ffa02590762aeb7912b93
Lipi Lee <lipisoft@gmail.com>
2017-06-20 15:59:18 +0900

    Simplify SessionTable with ConcurrentHashMap for thread safety

--- DataCollectorsClients/VpnCollector/app/src/main/java/com/att/arotcpcollector/SessionTable.java
+++ DataCollectorsClients/VpnCollector/app/src/main/java/com/att/arotcpcollector/SessionTable.java
@@ -18,106 +18,46 @@ package com.att.arotcpcollector;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
-import java.util.Collection;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Bharath.
  */
-
-class SessionTable implements Map<String, Session>{
+class SessionTable extends ConcurrentHashMap<String, Session> {
     private static final String TAG = "SessionTable";
 
     private final int limit;
-    private final Hashtable<String, Session> sessionTable = new Hashtable<>();
 
-    public SessionTable(int limit) {
+    SessionTable(int limit) {
         this.limit = limit;
     }
 
     @Override
-    public void clear() {
-        sessionTable.clear();
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return sessionTable.containsKey(key);
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return sessionTable.containsValue(value);
-    }
-
-    @NonNull
-    @Override
-    public Set<Entry<String, Session>> entrySet() {
-        return sessionTable.entrySet();
-    }
-
-    @Override
-    public Session get(Object key) {
-        return sessionTable.get(key);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return sessionTable.isEmpty();
-    }
-
-    @NonNull
-    @Override
-    public Set<String> keySet() {
-        return sessionTable.keySet();
-    }
-
-    @Override
-    public synchronized Session put(String key, Session value) {
-        if(sessionTable.size() >= limit) {
+    public Session put(@NonNull String key, @NonNull Session value) {
+        if(size() >= limit) {
             evictEntry();
         }
-        return sessionTable.put(key, value);
+
+        return super.put(key, value);
     }
 
     private void evictEntry() {
         long oldest = Long.MAX_VALUE;
         Session oldSession = null;
-        for(Entry<String, Session> se : sessionTable.entrySet()) {
-            Session session = se.getValue();
+
+        for(final Session session: values()) {
             if(oldest > session.getLastAccessed()) {
                 oldest = session.getLastAccessed();
                 oldSession = session;
             }
         }
+
         if(oldSession != null) {
-            Log.i(TAG, "Eviction initiated:" + oldSession.getSessionKey());
+            Log.i(TAG, "Eviction initiated: " + oldSession.getSessionKey());
             SessionManager.getInstance().closeSession(oldSession);
         } else {
             Log.e(TAG, "Error evicting entry");
         }
     }
-
-    @Override
-    public void putAll(@NonNull Map<? extends String, ? extends Session> map) {
-        throw new UnsupportedOperationException("This operation is not supported");
-    }
-
-    @Override
-    public Session remove(Object key) {
-        return sessionTable.remove(key);
-    }
-
-    @Override
-    public int size() {
-        return sessionTable.size();
-    }
-
-    @NonNull
-    @Override
-    public Collection<Session> values() {
-        return sessionTable.values();
-    }
 }
+
