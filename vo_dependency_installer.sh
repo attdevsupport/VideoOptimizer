5a46db83de34af38353bcd4028d35ff6ead3b1da
Barry Nelson <bn153x@att.com>
2023-08-29 14:59:02 -0700

    Update vo_dependency_installer.sh
    similar to version 10.0.0.2 plus additions from 1.0.1.0

--- vo_dependency_installer.sh
+++ vo_dependency_installer.sh
@@ -18,7 +18,7 @@
 
 #=======================================
 # vo_dependency_installer.sh
-VERSION="1.0.1.0"
+VERSION="1.0.1.1"
 #
 # Howto use this script:
 #   From your browser, select 'Save' and choose or create an empty folder. Make sure the script has the extension '.sh'.
@@ -38,8 +38,19 @@ VERSION="1.0.1.0"
 # 
 # It is advised that the computer is backed up, and important data is safely stored.
 # 
-# There are some basic requirements before this script should be run.
-# 1 You must have and run this under an Admin account, do not run under 'sudo'
+# The dependancies that are required include some 'HEAD' versions, which are not yet 
+# considered to be stable. 'libimobiledevice' is installed under HEAD, which may bring 
+# other dependancies, possibly also HEAD versions.
+# Some of the depenancies do not have functional formula to allow 'brew' to install them, 
+# and there for must be cloned from github and compiled.
+# 
+# These include 'ifuse' and 'libimobiledevice-glue'
+# see:
+#   git clone https://github.com/libimobiledevice/ifuse.git
+#   git clone https://github.com/libimobiledevice/libimobiledevice-glue.git
+# 
+# There are some basic requirements before this script can be run.
+# 1 You must have and run this under an Admin account, do not launch under 'sudo'
 # 2 macOS 13.4 or 13.4.1
 # 3 Xcode 14.3.1
 # 4 brew (aka HomeBrew https://brew.sh) 
@@ -58,7 +69,7 @@ echo "
 	then
 		brew update
 		brew upgrade
-		
+				
 		# make sure we can compile
 		brew install cmake
 		brew install automake
@@ -66,7 +77,7 @@ echo "
 		brew install autoconf
 		
 	else
-		echo "failded to find brew" >> error
+		echo "failed to find brew" >> error
 		exit_install
 	fi
 }
@@ -85,8 +96,60 @@ echo ">>>>> uninstall <<<<<"
 }
 
 #=======================================
+function compile_libimobiledevice-glue () {
+echo ""
+echo ">>>>> compile_libimobiledevice-glue <<<<<"
+
+	# install dependencies
+	# 1 libplist non-head version
+	
+	brew install --head libplist
+
+	if [ "${PWD##*/}" = "libimobiledevice-glue" ]; then
+		if [ "${ARCH_NAME}" = "x86_64" ]; then
+			touch "__compile_x86"
+			echo "./autogen.sh --prefix=/usr/local"
+ 			./autogen.sh --prefix=/usr/local
+		else
+			touch "__compile_aarch"
+			echo "./autogen.sh --prefix=/opt/homebrew"
+	 		./autogen.sh --prefix=/opt/homebrew
+		fi 
+		make
+		echo "sudo make install"
+		sudo make install
+	
+		if [ "${ARCH_NAME}" = "x86_64" ]; then
+			#For Intel / x86-64 machines - 
+			ln -s /usr/local/lib/pkgconfig/libimobiledevice-glue-1.0.pc /usr/local/opt/libplist/lib/pkgconfig/libimobiledevice-glue-1.0.pc 
+		else
+			#For M1 / aach64 machines - 
+			ln -s /opt/homebrew/lib/pkgconfig/libimobiledevice-glue-1.0.pc /opt/homebrew/opt/libplist/lib/pkgconfig/libimobiledevice-glue-1.0.pc  
+		fi
+	
+	else
+		echo "libimobiledevice-glue failure, in wrong path: `pwd`"
+		exit_install
+	fi
+}
+
+#=======================================
+function install_libimobiledevice () {
+echo ""
+echo ">>>>> install_libimobiledevice <<<<<"
+	
+	brew uninstall --ignore-dependencies libimobiledevice 2>/dev/null
+	
+	brew install --head usbmuxd
+	brew install --head libplist
+	brew install --head libimobiledevice
+	brew unlink libimobiledevice && brew link libimobiledevice
+}
+
+#=======================================
 # install ifuse
-# plus openssl 
+# plus openssl
+# on intel openssl requires non-head version of libimobiledevice
 function install_ifuse () {
 echo ""
 echo ">>>>> install_ifuse <<<<<"
@@ -95,8 +158,7 @@ echo ">>>>> install_ifuse <<<<<"
 	# 1 libimobiledevice non-head version for openssl
 	# 2 openssl
 	
-	brew install libimobiledevice
-
+	install_libimobiledevice
 	brew install openssl
 	
 	LISTING=$(brew list openssl)
@@ -135,14 +197,8 @@ echo ">>>>> install_ifuse <<<<<"
 	# now compile ifuse
 	./autogen.sh  
 	make
-	
-	if [ "$ARCH_NAME" == "x86_64" ];then
-		echo "make install"
-		make install
-	else
-		echo "sudo make install"
-		sudo make install
-	fi
+	echo "sudo make install"
+	sudo make install
 }
 
 #=======================================
@@ -158,24 +214,37 @@ echo ""
 	if [[ -d /Library/PreferencePanes/macFUSE.prefPane ]]; then
 	  init_brew
 	  do_uninstall
-
-	  echo "============================ compile iFuse ============================"
-	  rm -fr ifuse
-	  git clone https://github.com/libimobiledevice/ifuse.git
-	  # unzip ../bu/ifuse.zip -d ./ ; rm -fr __MACOSX
-	  if [ -d ifuse ]; then
-	  	( cd ifuse && install_ifuse )
-	  else
-	  	echo "clone: failed to git clone https://github.com/libimobiledevice/ifuse.git" >> error
-	  	exit_install
-	  fi
-	  
 	else
 	  echo "MacFuse not detected"
 	  echo "To install, visit https://osxfuse.github.io"
-	  exit_install
 	fi
 
+	echo "============================ libimobiledevice-glue ============================"
+	rm -fr libimobiledevice-glue
+	git clone https://github.com/libimobiledevice/libimobiledevice-glue.git
+	# unzip ../bu/libimobiledevice-glue.zip -d ./ ; rm -fr __MACOSX
+	if [ -d libimobiledevice-glue ]; then
+		( cd libimobiledevice-glue && compile_libimobiledevice-glue )
+	else
+		echo "clone: failed to git clone https://github.com/libimobiledevice/libimobiledevice-glue.git" >> error
+		exit_install
+	fi
+
+	echo "============================ compile iFuse ============================"
+	rm -fr ifuse
+	git clone https://github.com/libimobiledevice/ifuse.git
+	# unzip ../bu/ifuse.zip -d ./ ; rm -fr __MACOSX
+	if [ -d ifuse ]; then
+		( cd ifuse && install_ifuse )
+	else
+		echo "clone: failed to git clone https://github.com/libimobiledevice/ifuse.git" >> error
+		exit_install
+	fi
+
+	echo "============================ libimobiledevice HEAD version ============================"
+	if [ "${ARCH_NAME}" = "x86_64" ]; then
+		install_libimobiledevice
+	fi
 }
 #=======================================
 function exit_install (){
@@ -253,52 +322,39 @@ function machine_precheck (){
 	if [[ `which brew` == "" ]];then
 		echo "brew is not installed"
 		echo "To install, please visit: https://brew.sh"
-		CHECK="brew is not installed, To install, please visit: https://brew.sh"
-		return
+		exit_install
 	else
 		BREW_CONFIG=`brew config`
 	fi
 
-	if [[ -d /Library/PreferencePanes/macFUSE.prefPane ]]; then
-		echo "MacFuse is installed"
-	else
-		echo "MacFuse not detected"
-		echo "To install, visit https://osxfuse.github.io"
-		CHECK="fail:MacFuse not detected"
+
+# macOS: 13.2.1-arm64
+regex="(macOS): ([0-9]*)\.([0-9]*)\.([0-9]*)\-"
+check_version "$BREW_CONFIG" "$regex" macOS 13 2 1 13.2.1
+
+# CLT: 14.2.0.0.1.1668646533
+regex="(CLT): ([0-9A-Za-z/]*)\.([0-9]*)\.([0-9]*)"
+check_version "$BREW_CONFIG" "$regex" CLT 14 2 0 14.2.0 "CLT: 14.2.0.0.1.1668646533"
+
+# Xcode: 14.1
+regex="(Xcode): ([0-9]*)\.([0-9]*)"
+check_version "$BREW_CONFIG" "$regex" Xcode 14 1 "" "Xcode: 14.1"
+echo $CHECK
+
+if [[ ${CHECK} = "fail"* ]] ; then
+	if [[ $CHECK == *"CLT"* ]] ; then
+		echo "verify active developer directory with 'xcode-select -p'"
+		echo "use 'xcode-select --install' to install the correct commandline tools for your Xcode installation"
 	fi
-
-	# macOS: 13.2.1-arm64
-	regex="(macOS): ([0-9]*)\.([0-9]*)\.([0-9]*)\-"
-	check_version "$BREW_CONFIG" "$regex" macOS 13 2 1 13.2.1
-
-	# CLT: 14.2.0.0.1.1668646533
-	regex="(CLT): ([0-9A-Za-z/]*)\.([0-9]*)\.([0-9]*)"
-	check_version "$BREW_CONFIG" "$regex" CLT 14 3 0 14.2.0 "CLT: 14.3.0.0.1.1668646533"
-
-	# Xcode: 14.1
-	regex="(Xcode): ([0-9]*)\.([0-9]*)"
-	check_version "$BREW_CONFIG" "$regex" Xcode 14 3 "" "Xcode: 14.3"
-	echo $CHECK
-
-	if [[ ${CHECK} = "fail"* ]] ; then
-		if [[ $CHECK == *"CLT"* ]] ; then
-			echo "verify active developer directory with 'xcode-select -p'"
-			echo "use 'xcode-select --install' to install the correct commandline tools for your Xcode installation"
-		fi
-	else
-		echo "ready to proceed\!"
-	fi
+else
+	echo "ready to proceed\!"
+fi
 	
 }
 
 #-----------------------------------------------------
-# vo_dependency_installer.sh
+# vo_installer.sh
 #-----------------------------------------------------
-
-if [ "`id -u`" = "0" ]; then
-	echo "vo_dependency_installer.sh cannot be launched under sudo or from the root user"
-	exit 0
-fi 
 
 ARCH_NAME=`uname -m`
 PTH=`pwd |grep " "`
@@ -346,7 +402,7 @@ then
 	fi
 	# 
 else
-	echo "Please exit the folder 'libimobile_installation'"
+	echo "I am here `pwd`"
 	exit_install
 fi
 
